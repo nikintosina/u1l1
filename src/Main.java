7440a3255c6dd6fff311449915bd812f6973647b
nikintosina <manann.nikita@gmail.com>
2023-09-19 13:03:25 -0400

    wwwwww

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Nikita");
         System.out.println("Wolf of wall street");
+        System.out.println("shawarma");
 
     }
 }
