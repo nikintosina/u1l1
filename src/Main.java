7b6d71367fa6f9c288bc9f4375f171718c0b29f3
nikintosina <manann.nikita@gmail.com>
2023-09-19 12:49:30 -0400

    ggg

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nikita");
+        System.out.println("Wolf of wall street");
+
+    }
+}
+
+
